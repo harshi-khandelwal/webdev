f71a70ec8df5d8fec48594f822a744a124abd620
harshi-khandelwal <harshikhandelwal.12a@gmail.com>
2025-05-17 01:12:16 +0530

    day 3 - basics 1 (lec 1- 10)

---
+++ day 3/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "hitest@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountStatus;
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountStatus])
+
+/*
+prefer not to use var because of the issue in block scope and functional scope
+*/
+
---
+++ day 3/02_dataTypes.js
@@ -0,0 +1,36 @@
+// let score = null
+
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// //"33" => 33
+// //"33abc" => NaN
+
+// let isLoggedIn = 1
+// let booleanIsLogged = Boolean(isLoggedIn)
+// console.log(booleanIsLogged);
+
+// ************operations************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = "harshi"
+
+let str3 = str1 + str2
+console.log(str3)
+
+
+// ******************************************************
+
+// stack memory and heap memory
+
+// primitive type - stack memory 
+
+let myName = "harshi khandelwal"
+
+let anothername = myName
+
